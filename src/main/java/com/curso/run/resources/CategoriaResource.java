7d18e2b2874252794b6985226b5aa7e2237f4fcb
evalton <evalton.brt@gmail.com>
2019-03-30 14:21:46 -0300

    model Categoria

--- src/main/java/com/curso/run/resources/CategoriaResource.java
+++ src/main/java/com/curso/run/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.curso.run.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.run.model.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String lista() {
-		return "Listando categorias";
+	public List<Categoria> lista() {
+		Categoria cat1 = new Categoria(1L,"Informatica");
+		Categoria cat2 = new Categoria(2L,"Brinquedos");
+		
+		List<Categoria> lista= new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
 
 }
