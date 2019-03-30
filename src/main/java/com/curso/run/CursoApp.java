9f9816d181ff40d9c7fd26c785c448e43eec629d
evalton <evalton.brt@gmail.com>
2019-03-30 15:39:31 -0300

    instanciando objetos categoria

--- src/main/java/com/curso/run/CursoApp.java
+++ src/main/java/com/curso/run/CursoApp.java
@@ -1,13 +1,33 @@
 package com.curso.run;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.curso.run.model.Categoria;
+import com.curso.run.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoApp {
+public class CursoApp implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository catRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApp.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Brinquedos");
+		
+		catRepo.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
