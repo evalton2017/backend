146c4f08d1f1688900d3b37eb66576c107b849c5
evalton <evalton.brt@gmail.com>
2019-03-30 20:36:41 -0300

    model cliente, telefone e enderecos

--- src/main/java/com/curso/run/CursoApp.java
+++ src/main/java/com/curso/run/CursoApp.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.curso.run.model.Categoria;
 import com.curso.run.model.Cidade;
+import com.curso.run.model.Cliente;
+import com.curso.run.model.Endereco;
 import com.curso.run.model.Estado;
 import com.curso.run.model.Produto;
+import com.curso.run.model.enums.TipoCliente;
 import com.curso.run.repositories.CategoriaRepository;
 import com.curso.run.repositories.CidadeRepository;
+import com.curso.run.repositories.ClienteRepository;
+import com.curso.run.repositories.EnderecoRepository;
 import com.curso.run.repositories.EstadoRepository;
 import com.curso.run.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursoApp implements CommandLineRunner {
 	private EstadoRepository estRepo;
 	@Autowired
 	private CidadeRepository cidRepo;
+	@Autowired
+	private ClienteRepository cliRepo;
+	@Autowired
+	private EnderecoRepository endRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApp.class, args);
@@ -57,12 +66,12 @@ public class CursoApp implements CommandLineRunner {
 		catRepo.saveAll(Arrays.asList(cat1,cat2));
 		proRepo.saveAll(Arrays.asList(p1,p2,p3));
 		
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
+		Estado est1 = new Estado(null, "Goias");
+		Estado est2 = new Estado(null, "Distrito Federal");
 		
-		Cidade cid1 = new Cidade(null,"Uberlandia", est1);
-		Cidade cid2 = new Cidade(null,"Sorocaba", est2);
-		Cidade cid3 = new Cidade(null,"São Paulo", est2);
+		Cidade cid1 = new Cidade(null,"Goiania", est1);
+		Cidade cid2 = new Cidade(null,"Jardin Botanico", est2);
+		Cidade cid3 = new Cidade(null,"São Sebastiao", est2);
 		
 		est1.getCidades().addAll(Arrays.asList(cid1));
 		est2.getCidades().addAll(Arrays.asList(cid2,cid3));
@@ -70,6 +79,17 @@ public class CursoApp implements CommandLineRunner {
 		estRepo.saveAll(Arrays.asList(est1,est2));
 		cidRepo.saveAll(Arrays.asList(cid1,cid2,cid3));
 		
+		//Instanciar Clientes e endereços
+		Cliente cli1 = new Cliente(null,"Duke","duke@gmail.com","3256666699",TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("61-995662222","61-991383624"));
+				
+		Endereco e1 = new Endereco(null,"Quadra 14","29","","São jose","71693015",cli1,cid3);
+		Endereco e2 = new Endereco(null,"Quadra 15","19","","São jose","71693023",cli1,cid3);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		cliRepo.saveAll(Arrays.asList(cli1));
+		endRepo.saveAll(Arrays.asList(e1,e2));
 		
 		
 	}
