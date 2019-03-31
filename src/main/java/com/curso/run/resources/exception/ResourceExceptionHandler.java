5515704eb9d41edd4a25cedcfdf1ccc92d39cc04
evalton <evalton.brt@gmail.com>
2019-03-31 17:16:34 -0300

    personalizando validações

--- src/main/java/com/curso/run/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/curso/run/resources/exception/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,6 +15,7 @@ import com.curso.run.services.Exception.ObjectNotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+	//retorna o erro quando não encontrar o objeto
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandarError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		
@@ -20,11 +23,26 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 	
+	//retorna erro quando tentar excluir uma entidade que tiver relacionamento
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandarError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
 		
 		StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	
+	//retorna error de validacao
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandarError> dataIntegrity(MethodArgumentNotValidException e, HttpServletRequest request){
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		
+		for(FieldError er: e.getBindingResult().getFieldErrors()) {
+			err.addError(er.getField(), er.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	
 	
 }
