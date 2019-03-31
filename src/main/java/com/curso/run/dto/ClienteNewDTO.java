11f44eaad9f1cabd3f10f36a69e41f93c7d230c5
evalton <evalton.brt@gmail.com>
2019-03-31 20:50:35 -0300

    validação CPF e CNPJ cadastro cliente

--- src/main/java/com/curso/run/dto/ClienteNewDTO.java
+++ src/main/java/com/curso/run/dto/ClienteNewDTO.java
@@ -2,21 +2,40 @@ package com.curso.run.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.curso.run.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO  implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Preenchimento Obrigatorio")
+	@Length(min=5, max=80, message="O tamanho não pode ser menor que 5 e maior que 80")
 	private String nome;
+	@NotEmpty(message="Preenchimento Obrigatorio")
+	@Email(message="Email invalido")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento Obrigatorio")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento Obrigatorio")
 	private String logradouro;
+	@NotEmpty(message="Preenchimento Obrigatorio")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	@NotEmpty(message="Preenchimento Obrigatorio")
 	private String cep;
 
+	@NotEmpty(message="Preenchimento Obrigatorio")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
