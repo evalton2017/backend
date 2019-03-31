9f41fce1d3e440e75c120c0c99c4260a6a698d7d
evalton <evalton.brt@gmail.com>
2019-03-31 19:49:43 -0300

    inserir cliente com endereco e telefone

--- src/main/java/com/curso/run/resources/ClienteResources.java
+++ src/main/java/com/curso/run/resources/ClienteResources.java
@@ -1,5 +1,6 @@
 package com.curso.run.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.curso.run.dto.ClienteDTO;
+import com.curso.run.dto.ClienteNewDTO;
 import com.curso.run.model.Cliente;
 import com.curso.run.services.ClienteService;
 
@@ -32,6 +35,15 @@ public class ClienteResources {
 	public ResponseEntity<Cliente> buscar(@PathVariable Long id) throws ObjectNotFoundException {
 		Cliente cat = service.buscar(id);
 		return ResponseEntity.ok().body(cat);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO){
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				  .path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
--- src/main/java/com/curso/run/services/ClienteService.java
+++ src/main/java/com/curso/run/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.curso.run.dto.ClienteDTO;
+import com.curso.run.dto.ClienteNewDTO;
+import com.curso.run.model.Cidade;
 import com.curso.run.model.Cliente;
+import com.curso.run.model.Endereco;
+import com.curso.run.model.enums.TipoCliente;
 import com.curso.run.repositories.ClienteRepository;
+import com.curso.run.repositories.EnderecoRepository;
 import com.curso.run.services.Exception.DataIntegrityException;
 import com.curso.run.services.Exception.ObjectNotFoundException;
 
@@ -23,15 +29,20 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private EnderecoRepository endRepo;
+	
 	public Cliente buscar(Long id){
 		Optional<Cliente> cat = repo.findById(id);
 		return cat.orElseThrow(()-> new ObjectNotFoundException(
 				"Cliente não encontrada Id:"+id+", tipo: "+ Cliente.class.getName()));
 	}
 	
-	
+	@Transactional
 	public Cliente insert(Cliente obj) {
-		return repo.save(obj);
+		obj = repo.save(obj);
+		endRepo.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -62,6 +73,23 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null,objDTO.getNome(),objDTO.getEmail(),objDTO.getCpfOuCnpj(),TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null,objDTO.getLogradouro(),objDTO.getNumero(),objDTO.getComplemento(),objDTO.getBairro(),objDTO.getCep(), cli,cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if(objDTO.getTelefone2()!=null) {
+			cli.getTelefones().add(objDTO.getTelefone2());	
+		}
+		if(objDTO.getTelefone3()!=null) {
+			cli.getTelefones().add(objDTO.getTelefone3());	
+		}
+		
+		return cli;
+		
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
