e2028feb9f9716c06723642423b3defc299a6e25
evalton <evalton.brt@gmail.com>
2019-04-03 15:19:53 -0300

    inserindo pedido

--- src/main/java/com/curso/run/resources/PedidoResource.java
+++ src/main/java/com/curso/run/resources/PedidoResource.java
@@ -1,15 +1,22 @@
 package com.curso.run.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.curso.run.dto.CategoriaDTO;
+import com.curso.run.model.Categoria;
 import com.curso.run.model.Pedido;
 import com.curso.run.services.PedidoService;
 
@@ -26,6 +33,14 @@ public class PedidoResource {
 	public ResponseEntity<Pedido> buscar(@PathVariable Long id) throws ObjectNotFoundException {
 		Pedido cat = service.buscar(id);
 		return ResponseEntity.ok().body(cat);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				  .path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/curso/run/services/PedidoService.java
+++ src/main/java/com/curso/run/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.curso.run.services;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.curso.run.model.ItemPedido;
+import com.curso.run.model.PagamentoComBoleto;
 import com.curso.run.model.Pedido;
+import com.curso.run.model.enums.EstadoPagamento;
+import com.curso.run.repositories.ItemPedidoRepository;
+import com.curso.run.repositories.PagamentoRepository;
 import com.curso.run.repositories.PedidoRepository;
 import com.curso.run.services.Exception.ObjectNotFoundException;
 
@@ -16,10 +23,47 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService bolService;
+	
+	@Autowired
+	private PagamentoRepository pagRepo;
+	
+	@Autowired
+	private ProdutoService prodService;
+	
+	@Autowired
+	private ItemPedidoRepository itRepo;
+	
 	public Pedido buscar(Long id){
 		Optional<Pedido> cat = repo.findById(id);
 		return cat.orElseThrow(()-> new ObjectNotFoundException(
 				"Pedido não encontrada Id:"+id+", tipo: "+ Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			bolService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj=repo.save(obj);
+		pagRepo.save(obj.getPagamento());
+		for(ItemPedido ip:obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(prodService.buscar(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		
+		itRepo.saveAll(obj.getItens());
+		
+		return obj;
+		
+		
 	}
 	
 	
