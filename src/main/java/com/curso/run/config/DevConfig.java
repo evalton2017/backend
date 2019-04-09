49d3436c4206d1c20dea5797c79305a6536aa7fc
evalton <evalton.brt@gmail.com>
2019-04-09 16:01:02 -0300

    implementação smtp

--- src/main/java/com/curso/run/config/DevConfig.java
+++ src/main/java/com/curso/run/config/DevConfig.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.curso.run.services.SmtpEmailService;
 import com.curso.run.services.validation.DBService;
+import com.curso.run.services.validation.EmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,6 +31,11 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
