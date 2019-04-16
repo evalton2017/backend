9eef65b0b46970631c62307537b81e35e8f9ef0b
evalton <evalton.brt@gmail.com>
2019-04-15 21:04:16 -0300

    Autorização e token

--- src/main/java/com/curso/run/config/SecurityConfig.java
+++ src/main/java/com/curso/run/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.curso.run.Security.JWTAuthenticationFilter;
+import com.curso.run.Security.JWTAuthorizationFilter;
 import com.curso.run.Security.JWTUtil;
 
 /*Classe que realiza a configuração de segurança*/
@@ -62,6 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService ));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
